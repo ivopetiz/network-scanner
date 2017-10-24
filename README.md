75d382d0ed1b13358b53cbd9529d53b47c834fe6
Ivo Petiz <ivopetiz@gmail.com>
2017-10-24 22:03:07 +0100

    First working version

--- README.md
+++ README.md
@@ -1,2 +1,44 @@
-# go-portscanner
+Go-Portscanner
+==============
 TCP Network Port Scanner written in Go, nmap style. 
+## Installation
+```sh
+ $ git clone https://github.com/ivopetiz/portscanner.git portscanner
+ $ cd portscanner
+ $ go build portscanner.go portslist.go
+```
+## Examples
+Different ways to use Go-Portscanner.
+#### Get help
+```sh
+ $ portscanner -h
+ Usage of portscanner:
+   -A	Scans all ports from port 1 to 1024
+   -p  	Port or ports to scan (default "80")
+```
+Shows the output above, presenting the available options.
+
+#### Localhost Portscanner
+```sh
+$ portscanner 
+```
+Scans all local machine ports, from 1 to 1024.
+#### Network Machine Full Portscan
+```sh
+$ portscanner -p 1-65535 210.67.210.76
+```
+Scan all TCP ports from a network machine.
+#### Network Discovery
+```sh
+$ portscanner -p 21,80 192.168.0.1-192.168.0.254
+```
+Looks for HTTP and FTP servers on 192.168.0.0/24.
+
+#### Ping Servers
+```sh
+$ portscanner -p 22 113.213.200.101 4.0.75.4 84.188.238.94 11.2.224.214 153.194.246.247
+```
+Checks if SSH servers are up.
+## TODO
+* Improve performance.
+* Read IP:Port pair from file.
---
+++ portscanner.go
@@ -0,0 +1,245 @@
+
+package main
+
+import (
+		"os"
+		"fmt"
+		"net"
+		"flag"
+		"time"
+		"sync"
+		"regexp"
+		"strings"
+		"strconv"
+		)
+
+// IPv4 is the type used for IP addresses.
+type IPv4 [4]int
+
+// Check checks if an error is nil or a significant error. 
+func Check(err error) {
+
+	if err != nil {
+		panic(err)
+	}
+}
+
+// ToInt converts a string to integer, as strconv.Atoi does, but without 
+// returning errors.
+func ToInt(s string) int {
+
+	i, _ := strconv.Atoi(s)
+	return i
+}
+
+// PrintBanner presents a simple banner just to identify program name.
+func PrintBanner() {
+
+	fmt.Println("\n +----------------+")
+	fmt.Println(" | Go PortScanner |")
+	fmt.Println(" +----------------+")
+}
+
+// ToString converts an IP from IPv4 type to string.
+func (ip *IPv4)ToString() string {
+	
+	ip_stringed := strconv.Itoa(ip[0])
+	for i := 1; i < 4; i++ {
+		str_i := strconv.Itoa(ip[i])
+		ip_stringed += "." + str_i
+	}
+	return ip_stringed
+}
+
+// IsValid checks an IP address as valid or not.
+func (ip *IPv4)IsValid() bool { 
+
+	for i, oct := range ip {
+		if i == 0 || i == 3 {
+			if oct < 1 || oct > 255 {
+				return false
+			}
+		} else {
+			if oct < 1 || oct > 255 {
+				return false
+			}
+		}
+	}
+	return true
+}
+
+// PlusPlus increments an IPv4 value.
+func (ip *IPv4)PlusPlus() *IPv4 {
+
+	if ip[3] <= 254 {
+		ip[3] = ip[3] + 1
+	} else {
+		if ip[2] < 255 {
+			ip[2] = ip[2] + 1
+		} else {
+			if ip[1] < 255 {
+				ip[1] = ip[1] + 1
+			} else {
+				if ip[0] < 255 {
+					ip[0] = ip[0] + 1
+				}
+			}
+		}
+	}
+	return ip
+}
+
+// ToIPv4 converts an string to a IPv4.
+func ToIPv4(ip string) IPv4 {
+
+	var new_ip IPv4
+	
+	ip_s := strings.Split(ip, ".")
+	
+	for i, v := range (ip_s) {
+		new_ip[i], _ = strconv.Atoi(v)
+	}
+
+	return new_ip
+}
+
+// ParseIPSequence gets a sequence of IP addresses correspondent from an 
+// "init-end" entry.
+func ParseIPSequence(ip_sequence string) []IPv4 {
+
+	var array_ips []IPv4
+
+	series, _ := regexp.Compile("([0-9]+)")
+
+	// For sequence ips, using '-'
+	l_series := series.FindAllStringSubmatch(ip_sequence,-1)
+
+	for i := ToInt(l_series[3][0]); i <= ToInt(l_series[4][0]); i++ {
+		array_ips = append(array_ips, IPv4{
+								ToInt(l_series[0][0]), 
+								ToInt(l_series[1][0]), 
+								ToInt(l_series[2][0]), 
+								i})
+	}
+	return array_ips
+}
+
+// ParsePortList gets a port list from its port entry in arguments.
+func ParsePortList(raw_ports string) []string {
+
+	var ports []string
+
+	individuals, _ := regexp.Compile("([0-9]+)[,]*")
+	series, _ := regexp.Compile("([0-9]+)[-]([0-9]+)")
+
+	// For individual ports, separated by ','
+	l_individuais := individuals.FindAllStringSubmatch(raw_ports,-1)
+
+	// For sequence ports, using '-'
+	l_series := series.FindAllStringSubmatch(raw_ports,-1)
+
+	if len(l_series)>0 {
+		for _, s := range l_series {
+			init, _ := strconv.Atoi(s[1])
+			end, _  := strconv.Atoi(s[2])
+			for i := init; i < end; i++ {
+				ports = append(ports, strconv.Itoa(i))
+			}
+		}
+	}
+	for _, port := range l_individuais {
+		ports = append(ports, port[1])
+	}
+	return ports
+}
+
+// PresentResults presents all results in console.
+func PresentResults(open_ports []string) {
+
+	fmt.Println("Port:	Description:")
+	for _, port := range open_ports {
+		fmt.Println(port + "\t" + port_short_list[port])
+	}
+}
+
+// PortScanner scans IP:port pairs looking for open ports on IP addresses.
+func PortScanner(ip IPv4, port_list []string) []string {
+
+	var open 	[]string
+
+	for _, port := range port_list {
+
+		conn, err := net.DialTimeout("tcp", 
+								ip.ToString()+ ":"+ port, 
+								100 * time.Millisecond)
+
+		if err == nil {
+			conn.Close()
+			open = append(open, port)
+		}
+	}
+	return open
+}
+
+
+func main() {
+
+	start := time.Now()
+
+	var wg sync.WaitGroup
+	var ip 				IPv4
+	var ip_list 		[]IPv4
+	var port_list 		[]string
+
+	ports 		:= flag.String("p", "80", "Port or ports to scan")
+	all 		:= flag.Bool("A", false, "Scans from port 1 to 1024")
+	flag.Parse()
+
+	if *all {
+		*ports = "1-1024"
+	}
+
+	port_list = ParsePortList(*ports)
+	
+	PrintBanner()
+
+	if len(flag.Args()) == 0 {
+		ip_list = append(ip_list, IPv4{127,0,0,1})
+	
+	} else {
+		for _, ip_str := range flag.Args() {
+			
+			if strings.Contains(ip_str, "-") {
+				ip_list = append(ip_list, ParseIPSequence(ip_str)...) 
+			} else {
+				ip = ToIPv4(ip_str)
+				if ip.IsValid() {
+					ip_list = append(ip_list, ip)
+				}
+			}
+		}
+	}
+
+	// if there's not valid IPs to scan, system will exit with error.
+	if len(ip_list) == 0 {
+		fmt.Println("No valid IP addresses")
+		os.Exit(1)
+	} 
+
+	for _, ip := range ip_list {
+		wg.Add(1)
+		go func(ip IPv4){
+			defer wg.Done()
+			result := PortScanner(ip, port_list)
+			if len(result) > 0 {
+				fmt.Println("\n>" + ip.ToString())
+				PresentResults(result)
+			}
+		}(ip)
+	}
+
+	wg.Wait()
+
+	elapsed := time.Since(start)
+	fmt.Println("\nScanned in", elapsed)
+}
